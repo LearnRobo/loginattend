bd4e84de27894d211b759f2d147acbe397bc9083
Ashwini kumar Singh <ashwinirajput1991@gmail.com>
2026-05-15 12:58:17 +0530

    jai

--- admin/src/api/config.js
+++ admin/src/api/config.js
@@ -3,7 +3,7 @@
 const IS_PROD = import.meta.env.PROD;
 
 // When hosting, replace the 'render-url' with your actual Render backend link
-const PROD_URL = 'https://attendance-5seo.onrender.com'; 
+const PROD_URL = 'https://loginattend.onrender.com'; 
 const DEV_URL = 'http://192.168.88.251:5000';
 
 export const BASE_URL = IS_PROD ? PROD_URL : DEV_URL;
